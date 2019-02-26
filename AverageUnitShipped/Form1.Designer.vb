83948d0433c1665af2ffb5aba94ec467528de539
Mayank Gupta <mayankg2627@users.github.com>
2019-02-25 22:18:39 -0500

    vb.net code added

--- AverageUnitShipped/Form1.Designer.vb
+++ AverageUnitShipped/Form1.Designer.vb
@@ -22,10 +22,220 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.LblEmp3 = New System.Windows.Forms.Label()
+        Me.LblEmp2 = New System.Windows.Forms.Label()
+        Me.LblEmp1 = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnEnter = New System.Windows.Forms.Button()
+        Me.tbAvg = New System.Windows.Forms.TextBox()
+        Me.tbAvgEmp3 = New System.Windows.Forms.TextBox()
+        Me.tbAvgEmp2 = New System.Windows.Forms.TextBox()
+        Me.tbAvgEmp1 = New System.Windows.Forms.TextBox()
+        Me.tbstore3 = New System.Windows.Forms.TextBox()
+        Me.tbstore2 = New System.Windows.Forms.TextBox()
+        Me.tbstore1 = New System.Windows.Forms.TextBox()
+        Me.tbInput = New System.Windows.Forms.TextBox()
+        Me.lblunits = New System.Windows.Forms.Label()
+        Me.lblday = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'LblEmp3
+        '
+        Me.LblEmp3.AutoSize = True
+        Me.LblEmp3.Location = New System.Drawing.Point(394, 109)
+        Me.LblEmp3.Name = "LblEmp3"
+        Me.LblEmp3.Size = New System.Drawing.Size(78, 17)
+        Me.LblEmp3.TabIndex = 25
+        Me.LblEmp3.Text = "Employee3"
+        '
+        'LblEmp2
+        '
+        Me.LblEmp2.AutoSize = True
+        Me.LblEmp2.Location = New System.Drawing.Point(222, 106)
+        Me.LblEmp2.Name = "LblEmp2"
+        Me.LblEmp2.Size = New System.Drawing.Size(78, 17)
+        Me.LblEmp2.TabIndex = 22
+        Me.LblEmp2.Text = "Employee2"
+        '
+        'LblEmp1
+        '
+        Me.LblEmp1.AutoSize = True
+        Me.LblEmp1.Location = New System.Drawing.Point(57, 106)
+        Me.LblEmp1.Name = "LblEmp1"
+        Me.LblEmp1.Size = New System.Drawing.Size(78, 17)
+        Me.LblEmp1.TabIndex = 19
+        Me.LblEmp1.Text = "Employee1"
+        '
+        'btnExit
+        '
+        Me.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnExit.Location = New System.Drawing.Point(368, 440)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(108, 32)
+        Me.btnExit.TabIndex = 31
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.Location = New System.Drawing.Point(207, 440)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(108, 32)
+        Me.btnReset.TabIndex = 30
+        Me.btnReset.Text = "&Reset"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'btnEnter
+        '
+        Me.btnEnter.Location = New System.Drawing.Point(47, 440)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(108, 32)
+        Me.btnEnter.TabIndex = 29
+        Me.btnEnter.Text = "&Enter"
+        Me.btnEnter.UseVisualStyleBackColor = True
+        '
+        'tbAvg
+        '
+        Me.tbAvg.Location = New System.Drawing.Point(28, 377)
+        Me.tbAvg.Multiline = True
+        Me.tbAvg.Name = "tbAvg"
+        Me.tbAvg.ReadOnly = True
+        Me.tbAvg.Size = New System.Drawing.Size(469, 31)
+        Me.tbAvg.TabIndex = 28
+        Me.tbAvg.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'tbAvgEmp3
+        '
+        Me.tbAvgEmp3.Location = New System.Drawing.Point(356, 321)
+        Me.tbAvgEmp3.Multiline = True
+        Me.tbAvgEmp3.Name = "tbAvgEmp3"
+        Me.tbAvgEmp3.ReadOnly = True
+        Me.tbAvgEmp3.Size = New System.Drawing.Size(141, 31)
+        Me.tbAvgEmp3.TabIndex = 27
+        Me.tbAvgEmp3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'tbAvgEmp2
+        '
+        Me.tbAvgEmp2.Location = New System.Drawing.Point(191, 319)
+        Me.tbAvgEmp2.Multiline = True
+        Me.tbAvgEmp2.Name = "tbAvgEmp2"
+        Me.tbAvgEmp2.ReadOnly = True
+        Me.tbAvgEmp2.Size = New System.Drawing.Size(141, 31)
+        Me.tbAvgEmp2.TabIndex = 24
+        Me.tbAvgEmp2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'tbAvgEmp1
+        '
+        Me.tbAvgEmp1.Location = New System.Drawing.Point(28, 319)
+        Me.tbAvgEmp1.Multiline = True
+        Me.tbAvgEmp1.Name = "tbAvgEmp1"
+        Me.tbAvgEmp1.ReadOnly = True
+        Me.tbAvgEmp1.Size = New System.Drawing.Size(141, 31)
+        Me.tbAvgEmp1.TabIndex = 21
+        Me.tbAvgEmp1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'tbstore3
+        '
+        Me.tbstore3.Location = New System.Drawing.Point(356, 129)
+        Me.tbstore3.Multiline = True
+        Me.tbstore3.Name = "tbstore3"
+        Me.tbstore3.ReadOnly = True
+        Me.tbstore3.Size = New System.Drawing.Size(141, 165)
+        Me.tbstore3.TabIndex = 26
+        '
+        'tbstore2
+        '
+        Me.tbstore2.Location = New System.Drawing.Point(191, 129)
+        Me.tbstore2.Multiline = True
+        Me.tbstore2.Name = "tbstore2"
+        Me.tbstore2.ReadOnly = True
+        Me.tbstore2.Size = New System.Drawing.Size(141, 165)
+        Me.tbstore2.TabIndex = 23
+        '
+        'tbstore1
+        '
+        Me.tbstore1.Location = New System.Drawing.Point(28, 129)
+        Me.tbstore1.Multiline = True
+        Me.tbstore1.Name = "tbstore1"
+        Me.tbstore1.ReadOnly = True
+        Me.tbstore1.Size = New System.Drawing.Size(141, 165)
+        Me.tbstore1.TabIndex = 20
+        '
+        'tbInput
+        '
+        Me.tbInput.Location = New System.Drawing.Point(94, 55)
+        Me.tbInput.Multiline = True
+        Me.tbInput.Name = "tbInput"
+        Me.tbInput.Size = New System.Drawing.Size(93, 27)
+        Me.tbInput.TabIndex = 18
+        '
+        'lblunits
+        '
+        Me.lblunits.AutoSize = True
+        Me.lblunits.Location = New System.Drawing.Point(44, 58)
+        Me.lblunits.Name = "lblunits"
+        Me.lblunits.Size = New System.Drawing.Size(44, 17)
+        Me.lblunits.TabIndex = 17
+        Me.lblunits.Text = "Units:"
+        '
+        'lblday
+        '
+        Me.lblday.AutoSize = True
+        Me.lblday.Location = New System.Drawing.Point(43, 25)
+        Me.lblday.Name = "lblday"
+        Me.lblday.Size = New System.Drawing.Size(45, 17)
+        Me.lblday.TabIndex = 16
+        Me.lblday.Text = "Day 1"
+        '
+        'Form1
+        '
+        Me.AcceptButton = Me.btnEnter
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.CancelButton = Me.btnExit
+        Me.ClientSize = New System.Drawing.Size(541, 505)
+        Me.Controls.Add(Me.LblEmp3)
+        Me.Controls.Add(Me.LblEmp2)
+        Me.Controls.Add(Me.LblEmp1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnEnter)
+        Me.Controls.Add(Me.tbAvg)
+        Me.Controls.Add(Me.tbAvgEmp3)
+        Me.Controls.Add(Me.tbAvgEmp2)
+        Me.Controls.Add(Me.tbAvgEmp1)
+        Me.Controls.Add(Me.tbstore3)
+        Me.Controls.Add(Me.tbstore2)
+        Me.Controls.Add(Me.tbstore1)
+        Me.Controls.Add(Me.tbInput)
+        Me.Controls.Add(Me.lblunits)
+        Me.Controls.Add(Me.lblday)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents LblEmp3 As Label
+    Friend WithEvents LblEmp2 As Label
+    Friend WithEvents LblEmp1 As Label
+    Private WithEvents btnExit As Button
+    Private WithEvents btnReset As Button
+    Private WithEvents btnEnter As Button
+    Private WithEvents tbAvg As TextBox
+    Private WithEvents tbAvgEmp3 As TextBox
+    Private WithEvents tbAvgEmp2 As TextBox
+    Private WithEvents tbAvgEmp1 As TextBox
+    Friend WithEvents tbstore3 As TextBox
+    Friend WithEvents tbstore2 As TextBox
+    Friend WithEvents tbstore1 As TextBox
+    Private WithEvents tbInput As TextBox
+    Private WithEvents lblunits As Label
+    Private WithEvents lblday As Label
 End Class
--- AverageUnitShipped/Form1.vb
+++ AverageUnitShipped/Form1.vb
@@ -1,3 +1,198 @@
-﻿Public Class Form1
+﻿Option Strict On
+Public Class Form1
 
+    Const minimumShipped As Integer = 0
+    Const maximumShipped As Integer = 1000
+
+    Dim day As Integer = 1
+    Dim empId As Integer = 0
+
+    Dim units(2, 6) As Integer
+
+
+    Sub ResetForm()
+        ' reset variables
+        Dim Control As Control
+        Dim rt As TextBox
+        For Each Control In Me.Controls
+            If TypeOf (Control) Is TextBox Then
+                rt = CType(Control, TextBox)
+                rt.Clear()
+            End If
+
+        Next Control
+        tbAvg.Text = ""
+        day = 1
+        lblday.Text = "Day " + day.ToString
+        empId = 0
+        btnEnter.Enabled = True
+        tbInput.ReadOnly = False
+        LblEmp1.Font = New Font(LblEmp1.Font, FontStyle.Bold)
+        LblEmp2.Font = New Font(LblEmp2.Font, FontStyle.Regular)
+        LblEmp3.Font = New Font(LblEmp3.Font, FontStyle.Regular)
+
+    End Sub
+
+    Function arrayAverage(ByVal arrayToAverage As Integer(,)) As String
+        Dim Sum As Double = 0
+        Dim Avg As Double = 0
+        For ColumnIndex As Integer = 0 To units.GetUpperBound(1)
+            Sum = 0
+            For RowIndex As Integer = 0 To units.GetUpperBound(0)
+                Sum += units(RowIndex, ColumnIndex)
+            Next
+            Avg = Avg + (Sum / 7)
+        Next
+        Return Math.Round(Avg / 3, 2).ToString
+    End Function
+    Function DayInc(ByVal days As Integer) As String
+        Dim average As Double = 0
+        If (day < 7) Then
+            day = day + 1
+
+            lblday.Text = "Day " + day.ToString
+
+        ElseIf day = 7 Then
+            average = arrayAverage(units, empId)
+
+
+            empId = empId + 1
+            day = 1
+            lblday.Text = "Day " + day.ToString
+            Return "Average: " + average.ToString
+
+
+
+
+        End If
+        Return String.Empty
+    End Function
+
+
+    Function arrayAverage(ByVal arrayToAverage As Integer(,), emp As Integer) As Double
+        Dim Sum As Double = 0
+        For ColumnIndex As Integer = 0 To units.GetUpperBound(1)
+            Sum += units(emp, ColumnIndex)
+        Next
+
+        Return Math.Round(Sum / 7, 2)
+    End Function
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        Call ResetForm()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub btnEnter_Click(sender As Object, e As EventArgs) Handles btnEnter.Click
+        Dim UnitShipped As Integer
+        Dim userInput As String = tbInput.Text
+        Dim formattedOutput As String = ""
+
+
+        If Not Integer.TryParse(userInput, UnitShipped) Then
+            MessageBox.Show("Please ensure you enter a number for units Shipped!")
+
+            tbInput.SelectionStart = 0
+            tbInput.SelectionLength = tbInput.Text.Length
+
+        ElseIf (UnitShipped < minimumShipped OrElse UnitShipped > maximumShipped) Then
+
+            MessageBox.Show("Please ensure the value must be between 0 to 1000!")
+
+        Else
+            units(empId, day - 1) = Convert.ToInt32(userInput)
+
+            For counter As Integer = 1 To day
+                formattedOutput += units(empId, counter - 1).ToString + vbCrLf
+                tbInput.Text = ""
+
+            Next
+
+            If empId = 0 Then
+
+                tbstore1.Text = formattedOutput
+                formattedOutput = String.Empty
+                tbAvgEmp1.Text = DayInc(day)
+            ElseIf empId = 1 Then
+                tbstore2.Text = formattedOutput
+                formattedOutput = String.Empty
+                tbAvgEmp2.Text = DayInc(day)
+            Else
+
+                tbstore3.Text = formattedOutput
+                formattedOutput = String.Empty
+
+                tbAvgEmp3.Text = DayInc(day)
+
+            End If
+
+        End If
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LblEmp1.Font = New Font(LblEmp1.Font, FontStyle.Bold)
+    End Sub
+
+    Private Sub tbAvgEmp1_TextChanged(sender As Object, e As EventArgs) Handles tbAvgEmp1.TextChanged
+        LblEmp1.Font = New Font(LblEmp1.Font, FontStyle.Regular)
+        LblEmp2.Font = New Font(LblEmp2.Font, FontStyle.Bold)
+
+    End Sub
+
+    Private Sub tbAvgEmp2_TextChanged(sender As Object, e As EventArgs) Handles tbAvgEmp2.TextChanged
+        LblEmp2.Font = New Font(LblEmp2.Font, FontStyle.Regular)
+        LblEmp3.Font = New Font(LblEmp3.Font, FontStyle.Bold)
+    End Sub
+
+    Private Sub tbAvgEmp3_TextChanged(sender As Object, e As EventArgs) Handles tbAvgEmp3.TextChanged
+        btnEnter.Enabled = False
+        tbInput.ReadOnly = True
+        tbAvg.Text = "Average per day= " + arrayAverage(units)
+        lblday.Text = "Done"
+        ' tbAvg.Text = (Math.Round((Convert.ToDouble(tbAvgEmp1.Text) + Convert.ToDouble(tbAvgEmp2.Text) + Convert.ToDouble(tbAvgEmp3.Text)) / 3, 2)).ToString
+    End Sub
+
+    Private Sub tbstore1_TextChanged(sender As Object, e As EventArgs) Handles tbstore1.TextChanged
+
+    End Sub
+
+    Private Sub lblunits_Click(sender As Object, e As EventArgs) Handles lblunits.Click
+
+    End Sub
+
+    Private Sub tbInput_TextChanged(sender As Object, e As EventArgs) Handles tbInput.TextChanged
+
+    End Sub
+
+    Private Sub lblday_Click(sender As Object, e As EventArgs) Handles lblday.Click
+
+    End Sub
+
+    Private Sub LblEmp1_Click(sender As Object, e As EventArgs) Handles LblEmp1.Click
+
+    End Sub
+
+    Private Sub LblEmp2_Click(sender As Object, e As EventArgs) Handles LblEmp2.Click
+
+    End Sub
+
+    Private Sub tbstore2_TextChanged(sender As Object, e As EventArgs) Handles tbstore2.TextChanged
+
+    End Sub
+
+    Private Sub LblEmp3_Click(sender As Object, e As EventArgs) Handles LblEmp3.Click
+
+    End Sub
+
+    Private Sub tbstore3_TextChanged(sender As Object, e As EventArgs) Handles tbstore3.TextChanged
+
+    End Sub
+
+    Private Sub tbAvg_TextChanged(sender As Object, e As EventArgs) Handles tbAvg.TextChanged
+
+    End Sub
 End Class
